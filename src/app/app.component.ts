fae899da78f2e46449375ecce70bd38847403a44
Solhack <nsetshedi.ss@gmail.com>
2020-12-21 00:25:03 +0200

    bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,15 @@ export class AppComponent {
   title = 'socgamify';
 }
 
-var formatThousandsNoRounding = function(n, dp){
-  var e = '', s = e+n, l = s.length, b = n < 0 ? 1 : 0,
-      i = s.lastIndexOf(','), j = i == -1 ? l : i,
-      r = e, d = s.substr(j+1, dp);
-  while ( (j-=3) > b ) { r = '.' + s.substr(j, 3) + r; }
-  return s.substr(0, j + 3) + r + 
-    (dp ? ',' + d + ( d.length < dp ? 
-        ('00000').substr(0, dp - d.length):e):e);
-};
+//var formatThousandsNoRounding = function(n, dp){
+ // var e = '', s = e+n, l = s.length, b = n < 0 ? 1 : 0,
+ //     i = s.lastIndexOf(','), j = i == -1 ? l : i,
+//      r = e, d = s.substr(j+1, dp);
+///  while ( (j-=3) > b ) { r = '.' + s.substr(j, 3) + r; }
+//  return s.substr(0, j + 3) + r + 
+//    (dp ? ',' + d + ( d.length < dp ? 
+//        ('00000').substr(0, dp - d.length):e):e);
+//};
 
 //var hasRun = false;
 
